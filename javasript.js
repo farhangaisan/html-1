29db7c8451d990005e50d0fb3ccd28cd3904d992
farhangaisan <farhangaisan>
2018-11-17 14:17:42 +0700

    belajar java

---
+++ javasript.js
@@ -0,0 +1,16 @@
+window.prompt("input");
+function Pagi();
+{
+    document.getElementById("pagi")
+    event.preventDefault();
+}
+function siang();
+{
+    document.getElementById("siang")
+    event.preventDefault();
+}
+function malam();
+{
+    document.getElementById("malam");
+    event.preventDefault();
+}
