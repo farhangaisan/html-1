6b90ed6da0c467db077f41883e1500e162aadf86
farhangaisan <farhangaisan>
2018-11-19 00:16:30 +0700

    gallery

--- javasript.js
+++ javasript.js
@@ -1,15 +1,45 @@
+function pagi() {
+    document.getElementById("pagi").innerHTML = "Selamat Pagi Netizen"
 
-function pagi()
-{
-    document.getElementById("pagi").innerHTML="Selamat Pagi Netizen"
-    
 }
-function siang()
-{
+
+function siang() {
     document.getElementById("pagi").innerHTML = "Selamat Siang Netizen"
 
 }
-function malam()
-{
-    document.getElementById("pagi").innerHTML="Selamat Malam Netizen"
+
+function malam() {
+    document.getElementById("pagi").innerHTML = "Selamat Malam Netizen"
+}
+
+function picture1() {
+    document.getElementById("name").innerHTML = "Harley Davidson Night V-Rod"
+}
+
+function picture2() {
+    document.getElementById("name").innerHTML = "Kawasaki Ninja H2R"
+}
+
+function picture3() {
+    document.getElementById("name").innerHTML = "Ducati Scrambler 800"
+}
+
+function picture4() {
+    document.getElementById("name").innerHTML = "Ducati Scrambler Sixty2"
+}
+
+function picture5() {
+    document.getElementById("name").innerHTML = "BMW K100RS CAFERACER"
+}
+
+function picture6() {
+    document.getElementById("name").innerHTML = "Suzuki GSX-S 1000"
+}
+
+function picture7() {
+    document.getElementById("name").innerHTML = "HONDA CBR1000RR"
+}
+
+function picture8() {
+    document.getElementById("name").innerHTML = "Yamaha R6"
 }
